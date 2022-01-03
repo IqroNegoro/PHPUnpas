1833e415b2dae14d388a69b202e1a05a0961e731
IqroNegoro <iqronegoro0@gmail.com>
2022-01-03 17:15:25 +0700

    MAGHRIB

--- OOP/produk.php
+++ OOP/produk.php
@@ -5,16 +5,32 @@ class Produk {
     public $penulis;
     public $penerbit;
     public $harga;
+    public $halaman;
+    public $waktu;
+    public $type;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $halaman = 0, $waktu = 0, $type) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+        $this->halaman = $halaman;
+        $this->waktu = $waktu;
+        $this->type = $type;
     }
 
     public function getLabel() {
-        return "$this->penulis, $this->penerbit";
+        return "{$this->judul} | {$this->penulis}, {$this->penerbit} (Rp. {$this->harga})";
+    }
+
+    public function getInfoLengkap() {
+        $str = "{$this->type} : {$this->getLabel()}";
+        if ($this->type == "Komik") {
+            $str .= " - {$this->halaman} Halaman.";
+        } else if ($this->type == "Game") {
+            $str .= " - {$this->waktu} Jam";
+        }
+        return $str;
     }
     
 }
@@ -30,9 +46,29 @@ class CetakInfo {
 
 // $produk2 = new Produk();
 // $produk2->judul = "Uncharted";
-$produk3 = new Produk("Naruto", "Masashi Kishimoto", "Shounen Jump", 30000);
-echo "Komik: " . $produk3->getLabel();
-echo "<br>";
-$info = new CetakInfo();
-echo $info->cetak($produk3);
-?>
+$produk3 = new Produk("Naruto", "Masashi Kishimoto", "Shounen Jump", 30000, 100, 0, "Komik");
+$produk4 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
+// $info = new CetakInfo();
+// echo $info->cetak($produk3);
+
+// Komik: Naruto | Masashi Kishimoto, Shounen Jump (Rp. 30000) - 100 Halaman;
+// Game : Uncharted | Neil Druckmann, Sony Computer (Rp. 250000) - 50 JUam;
+echo $produk3->getInfoLengkap();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
