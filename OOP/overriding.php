b7fe3cfda1ffc68b67725106ea9a0fa66b5e17b2
IqroNegoro <iqronegoro0@gmail.com>
2022-01-04 10:22:02 +0700

    We not riding with u. but we gonna ride u

--- OOP/overriding.php
+++ OOP/overriding.php
@@ -7,32 +7,37 @@ class Produk {
     public $harga;
     public $halaman;
     public $waktu;
-    public $type;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $halaman = 0, $waktu = 0, $type) {
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $halaman = 0, $waktu = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
         $this->halaman = $halaman;
         $this->waktu = $waktu;
-        $this->type = $type;
     }
 
     public function getLabel() {
-        return "{$this->judul} | {$this->penulis}, {$this->penerbit} (Rp. {$this->harga})";
+        return "{$this->penulis}, {$this->penerbit}";
     }
 
     public function getInfoLengkap() {
-        $str = "{$this->type} : {$this->getLabel()}";
-        if ($this->type == "Komik") {
-            $str .= " - {$this->halaman} Halaman.";
-        } else if ($this->type == "Game") {
-            $str .= " - {$this->waktu} Jam";
-        }
+        $str = "{$this->judul} | {$this->getLabel()} {$this->harga}";
         return $str;
     }
     
+}
+
+class Komik extends Produk {
+    public function getInfoKomik() {
+        return "Komik : {$this->getInfoLengkap()} - {$this->halaman} Halaman";
+    }
+}
+
+class Game extends Produk {
+    public function getInfoGame() {
+        return "Game : {$this->getInfoLengkap()} - {$this->waktu} Jam";
+    }
 }
 
 class CetakInfo {
@@ -46,15 +51,16 @@ class CetakInfo {
 
 // $produk2 = new Produk();
 // $produk2->judul = "Uncharted";
-$produk3 = new Produk("Naruto", "Masashi Kishimoto", "Shounen Jump", 30000, 100, 0, "Komik");
-$produk4 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
+$produk3 = new Komik("Naruto", "Masashi Kishimoto", "Shounen Jump", 30000, 100, 0);
+$produk4 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
 // $info = new CetakInfo();
 // echo $info->cetak($produk3);
 
 // Komik: Naruto | Masashi Kishimoto, Shounen Jump (Rp. 30000) - 100 Halaman;
 // Game : Uncharted | Neil Druckmann, Sony Computer (Rp. 250000) - 50 JUam;
-echo $produk3->getInfoLengkap();
-
+echo $produk3->getInfoKomik();
+echo "<br>";
+echo $produk4->getInfoGame();
 
 
 
