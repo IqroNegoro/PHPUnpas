29f697e97b3d713a3aa1d616e5c7c576e1893609
IqroNegoro <iqronegoro0@gmail.com>
2022-01-05 11:31:35 +0700

    Constanta Function

--- OOP/constanct.php
+++ OOP/constanct.php
@@ -1,5 +1,24 @@
 <?php 
 
-define("NAMA", "Sandhika galih");
+// define("NAMA", "Sandhika galih");
+// echo NAMA;
+
+// const UMUR = 32;
+// echo UMUR;
+
+class Coba {
+    public static $kelas = __CLASS__;
+}
+
+echo Coba::$kelas;
+
+
+// echo Coba::NAMA;
+echo __LINE__;
+echo __FILE__;
+
+function aokwkoawokaw() {
+    return __FUNCTION__;
+}
 
 ?>
